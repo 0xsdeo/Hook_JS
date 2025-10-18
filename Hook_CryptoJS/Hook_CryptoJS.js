31f65bb78cc85858e26632d71ff05d7dc69f9132
0xsdeo <i0xsdeo@foxmail.com>
2025-10-18 12:34:07 +0800

    Update Hook_CryptoJS.js

--- Hook_CryptoJS/Hook_CryptoJS.js
+++ Hook_CryptoJS/Hook_CryptoJS.js
@@ -4,7 +4,8 @@
 // @version      2025-10-17
 // @description  Hook CryptoJS
 // @author       0xsdeo
-// @match        http://*/*
+// @run-at       document-start
+// @match        *
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -12,18 +13,7 @@
 (function () {
     'use strict';
 
-    // let temp_toString = Function.prototype.toString;
-    //
-    // Function.prototype.toString = function () {
-    //     if (this === Function.prototype.toString) {
-    //         return 'function toString() { [native code] }';
-    //     } else if (this === xxx) { // 将xxx修改为要hook的方法
-    //         return ''; // 在控制台执行xxx.toString()，将输出的内容替换掉空字符串
-    //     }
-    //     return temp_toString.apply(this, arguments);
-    // }
-
-    function hasprop(obj) {
+    function hasEncryptProp(obj) {
         const requiredProps = [
             'ciphertext',
             'key',
@@ -50,10 +40,31 @@
         return true;
     }
 
+    function hasDecryptProp(obj) {
+        const requiredProps = [
+            'sigBytes',
+            'words'
+        ];
+
+        // 检查对象是否存在且为对象类型
+        if (!obj || typeof obj !== 'object') {
+            return false;
+        }
+
+        // 检查所有必需属性是否存在
+        for (const prop of requiredProps) {
+            if (!(prop in obj)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     let temp_apply = Function.prototype.apply;
 
     Function.prototype.apply = function () {
-        if (arguments.length === 2 && arguments[1] && typeof arguments[1] === 'object' && arguments[1].length === 1 && hasprop(arguments[1][0])) {
+        if (arguments.length === 2 && arguments[1] && typeof arguments[1] === 'object' && arguments[1].length === 1 && hasEncryptProp(arguments[1][0])) {
             if (Object.hasOwn(arguments[0],"$super") && Object.hasOwn(arguments[0],"init")) {
                 if (this.toString().indexOf('function()') !== -1) {
                     console.log(...arguments);
@@ -64,6 +75,22 @@
                     }
                     else{
                         console.log("加密时未用到iv")
+                    }
+                }
+            }
+        }
+        else if (arguments.length === 2 && arguments[1] && typeof arguments[1] === 'object' && arguments[1].length === 3 && hasDecryptProp(arguments[1][1])){
+            console.log(arguments)
+            if (Object.hasOwn(arguments[0],"$super") && Object.hasOwn(arguments[0],"init")) {
+                if (this.toString().indexOf('function()') !== -1) {
+                    let key = arguments[1][1];
+                    console.log("解密Hex key：",key.toString());
+                    if (Object.hasOwn(arguments[1][2],"iv")) {
+                        let iv = arguments[1][2]["iv"];
+                        console.log("解密Hex iv：",iv.toString());
+                    }
+                    else {
+                        console.log("解密时未用到iv")
                     }
                 }
             }
