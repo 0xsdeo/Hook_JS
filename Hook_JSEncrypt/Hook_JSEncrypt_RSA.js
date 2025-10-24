913f60d1a85cc5ecdf305aa5fb026cb7231d51b1
0xsdeo <i0xsdeo@foxmail.com>
2025-10-25 00:49:35 +0800

    Update Hook_JSEncrypt_RSA.js

--- Hook_JSEncrypt/Hook_JSEncrypt_RSA.js
+++ Hook_JSEncrypt/Hook_JSEncrypt_RSA.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hook_JSEncrypt_RSA
 // @namespace    https://github.com/0xsdeo/Hook_JS
-// @version      2025-10-17
+// @version      2025-10-24
 // @description  Hook JSEncrypt RSA
 // @author       0xsdeo
 // @run-at       document-start
@@ -44,8 +44,8 @@
     let temp_call = Function.prototype.call;
 
     Function.prototype.call = function () {
-        if (arguments.length === 1 && arguments[0] && typeof arguments[0].__proto__ === 'object' && hasRSAProp(arguments[0].__proto__)) {
-            if ("__proto__" in arguments[0].__proto__ && Object.hasOwn(arguments[0].__proto__.__proto__, "encrypt")) {
+        if (arguments.length === 1 && arguments[0] && arguments[0].__proto__ && typeof arguments[0].__proto__ === 'object' && hasRSAProp(arguments[0].__proto__)) {
+            if ("__proto__" in arguments[0].__proto__ && arguments[0].__proto__.__proto__ && Object.hasOwn(arguments[0].__proto__.__proto__, "encrypt") && Object.hasOwn(arguments[0].__proto__.__proto__, "decrypt")) {
                 if (arguments[0].__proto__.__proto__.encrypt.toString().indexOf('RSA加密') === -1) {
 
                     let temp_encrypt = arguments[0].__proto__.__proto__.encrypt;
@@ -57,6 +57,20 @@
                         console.log("RSA加密 原始数据：", ...arguments);
                         console.log("RSA加密 密文：", encrypt_text);
                         return encrypt_text;
+                    }
+                }
+
+                if (arguments[0].__proto__.__proto__.decrypt.toString().indexOf('RSA解密') === -1) {
+
+                    let temp_decrypt = arguments[0].__proto__.__proto__.decrypt;
+
+                    arguments[0].__proto__.__proto__.decrypt = function () {
+                        let decrypt_text = temp_decrypt.bind(this, ...arguments)();
+
+                        console.log("RSA 私钥：\n", this.getPrivateKey());
+                        console.log("RSA解密 原始数据：", ...arguments);
+                        console.log("RSA解密 明文：", decrypt_text);
+                        return decrypt_text;
                     }
                 }
             }
