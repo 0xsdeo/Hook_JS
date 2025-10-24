5d73cbecc8d6a0b1cb61391c63f6e01a53527f4d
0xsdeo <i0xsdeo@foxmail.com>
2025-10-24 22:49:59 +0800

    Create Hook_JSEncrypt_RSA.js

---
+++ Hook_JSEncrypt/Hook_JSEncrypt_RSA.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         Hook_JSEncrypt_RSA
+// @namespace    https://github.com/0xsdeo/Hook_JS
+// @version      2025-10-17
+// @description  Hook JSEncrypt RSA
+// @author       0xsdeo
+// @run-at       document-start
+// @match        *
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function hasRSAProp(obj) {
+        const requiredProps = [
+            'constructor',
+            'getPrivateBaseKey',
+            'getPrivateBaseKeyB64',
+            'getPrivateKey',
+            'getPublicBaseKey',
+            'getPublicBaseKeyB64',
+            'getPublicKey',
+            'parseKey',
+            'parsePropertiesFrom'
+        ];
+
+        // 检查对象是否存在且为对象类型
+        if (!obj || typeof obj !== 'object') {
+            return false;
+        }
+
+        // 检查所有必需属性是否存在
+        for (const prop of requiredProps) {
+            if (!(prop in obj)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    let temp_call = Function.prototype.call;
+
+    Function.prototype.call = function () {
+        if (arguments.length === 1 && arguments[0] && typeof arguments[0].__proto__ === 'object' && hasRSAProp(arguments[0].__proto__)) {
+            if ("__proto__" in arguments[0].__proto__ && Object.hasOwn(arguments[0].__proto__.__proto__, "encrypt")) {
+                if (arguments[0].__proto__.__proto__.encrypt.toString().indexOf('RSA加密') === -1) {
+
+                    let temp_encrypt = arguments[0].__proto__.__proto__.encrypt;
+
+                    arguments[0].__proto__.__proto__.encrypt = function () {
+                        let encrypt_text = temp_encrypt.bind(this, ...arguments)();
+
+                        console.log("RSA 公钥：\n", this.getPublicKey());
+                        console.log("RSA加密 原始数据：", ...arguments);
+                        console.log("RSA加密 密文：", encrypt_text);
+                        return encrypt_text;
+                    }
+                }
+            }
+        }
+        return temp_call.bind(this, ...arguments)();
+    }
+})();
